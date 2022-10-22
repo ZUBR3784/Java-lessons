dcde3ea547711c651d4022b67f3d13d77e558c78
ZUBR3784 <uladimirzhlabo@gmail.com>
2022-10-23 00:53:07 +0200

    h

--- src/klasswork/klasswork10/Klasswork10.java
+++ src/klasswork/klasswork10/Klasswork10.java
@@ -107,16 +107,16 @@ public class Klasswork10 {
 
 
         //рисуем треугольник
-//        int hight =10;
-//        for (int i = 0; i <hight ; i++) {
-//            for (int j = 0; j <hight -1 ; j++) {
-//                System.out.print(" ");
-//            }
-//            for (int j = 0; j <i*2 - 1 ; j++) {
-//                System.out.print("*");
-//            }
-//            System.out.println("");
-//        }
+        int hight =10;
+        for (int i = 0; i <hight ; i++) {
+            for (int j = 0; j <hight -i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <i*2 - 1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
 
 
 
