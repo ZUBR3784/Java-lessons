c692fbc5d68907e71757a84c471aaaabb0e9ac84
ZUBR3784 <uladimirzhlabo@gmail.com>
2022-10-25 22:03:59 +0200

    classWork11

---
+++ src/klasswork/klasswork11/Klasswork11.java
@@ -0,0 +1,97 @@
+package klasswork.klasswork11;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Klasswork11 {
+    public static void main(String[] args) {
+//        int[] array = new int[10];
+//        int[] documents = {12, 34, 65, 22};
+//        for (int i = 0; i < documents.length; i++) {
+//            System.out.println(documents[i]);
+//        }
+//        System.out.println(documents[0]);
+//        System.out.println(documents[1]);
+//        System.out.println(documents[2]);
+//        System.out.println(documents[3]);
+//        for (int i = 0; i < array.length ; i++) {
+//            if (i % 2 == 0){
+//                array[i]=1;
+//            }else {
+//                array[i]=2;
+//            }
+//            System.out.println("index: " +i+" ,val: "+array[i]);
+//        }
+//        String[] pocketThings = new String[3];
+//        pocketThings[2] = "cake";
+//        for (int i = 0; i < pocketThings.length; i++) {
+//            System.out.println(pocketThings[i]);
+//        }
+//        int[] arryOne = new int[2];
+//        arryOne[0]=1;
+//        arryOne[1]=48;
+//
+//        int[] arryTwo =new int[3];
+//        for (int i = 0; i <arryOne.length; i++) {// or-> int[] arrayThree = Arrays.copyOf(arryOne,6);
+//            arryTwo[i]=arryOne[i];
+//
+////            int[] arrayFour = Arrays.copyOfRange(arryOne, 2,6);//копировать данные 1-го массива в конкретном промежутке
+//        }
+//        arryTwo[2]=56;
+//        int[]arrayFive = arryOne.clone();//создание копии
+//        arrayFive[1]=1;
+//        Arrays.sort(arryTwo);
+//
+//
+//        System.out.println(Arrays.toString(arryOne));
+//        System.out.println(Arrays.toString(arrayFive));
+//        int howManyNum = 100;
+//        int[] array = new int[howManyNum/2];
+//        int j = 0;
+//        for (int i = 0; i < howManyNum; i++) {
+//            if (i%2==1){
+//                array[j]=i;
+//                j++;
+//            }
+//        }
+//        int[] array2 = new  int[howManyNum/2];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            array2[i]= array[array.length-1-i];
+//        }
+//        System.out.println(Arrays.toString(array));
+//        System.out.println(Arrays.toString(array2));
+
+
+
+
+
+
+
+//        int[] array = new int[15];
+//        Random random=new Random();
+//        for (int i = 0; i < args.length; i++) {
+//            array[i] = random.nextInt(10);
+//        }
+//        System.out.println(Arrays.toString(array));
+//        int max = 0;
+//        for (int i = 0; i <array.length ; i++) {
+//            if (max< array[i]){
+//                max=array[i];
+//            }
+//        }
+//        System.out.println(max);
+
+
+
+
+//        String[] veekNames = {"Monday","Thuesday","Wendsday","T","F","S","","",""};
+
+
+
+
+
+        
+    }
+}
+
